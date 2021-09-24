aa6a3e5d3010b699e2de5802115f40b63b365c24
Joel Hopper <hopper.joel.w@gmail.com>
2021-09-23 20:45:15 -0600

    updates

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-const links = [
-    {
-        label: "Week1 notes",
-        url: "week1/index.html"
-    }
-]
+const links = {
+    week2: {
+        label: "Week2 notes",
+        url: "studynotes.html"
+    },
+    week3: {}
+};
 
+function assignmentUpdate(links) {
+    const assignmentUl = document.getElementById("assignment-list");
+    for (let i = 0; i < links.length; i ++) {
+        
+        let listAssignment = assignmentUl.innerHTML(`<li>${links[i].label}`)
+    } 
+}
+
+function testAssignments(links) {
+    const 
+    for (const key of Object.keys(links)) {
+        console.log(`${key} => ${links[key]}`);
+    }
+}
