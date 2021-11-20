5efca6b0d4858a1ca7287bbc9bfbe148357b3c4d
Joel Hopper <hopper.joel.w@gmail.com>
2021-11-19 17:06:07 -0700

    sd

--- week10/earthquake watch/main.js
+++ week10/earthquake watch/main.js
@@ -1,13 +1,7 @@
 
-import { getJSON, getLocation, getLocationTest, fThisShiz, iHateThis} from "./utilities.js";
+import { getJSON, getLocation, getLocationTest} from "./utilities.js";
 
 const baseURL = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2019-01-01&endtime=2019-02-02';
 
-let getStuff = getJSON(baseURL);
-let location = getLocationTest();
 
-console.log(iHateThis());
-
-let test3 = fThisShiz();
-console.log(fThisShiz);
 
--- week10/earthquake watch/utilities.js
+++ week10/earthquake watch/utilities.js
@@ -1,23 +1,21 @@
-
 export function getJSON(url) {
-    fetch(url)
-    .then((response) => {if (!response.ok) {
-        throw Error(response.statusText);
-    } else {
-        return response.json();
-    }
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
-
-export const getLocation = function(options) {
-    response = new Promise(function(resolve, reject) {
-        navigator.geolocation.getCurrentPosition(resolve, reject, options);
-    })
-
-}
+    return fetch(url)
+      .then(function(response) {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        } else {
+          return response.json();
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+  export const getLocation = function(options) {
+    return new Promise(function(resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+  };
 
 export const getLocationTest = function() {
     if (navigator.geolocation) {
@@ -32,27 +30,7 @@ export const getLocationTest = function() {
     }
 }
 
-export const fThisShiz = function() {
-    navigator.geolocation.getCurrentPosition((position) => {
-        return displayPosition(position.coords.latitude, position.coords.longitude);
-        
-    });
-    
-}
 
-function displayPosition(latitude, longitude) {
-    let string = `&latitude=${latitude}&longitude=${longitude}&maxradiuskm=100`;
-    return string;
-}
 
-export function callbackClosure(i, callback) {
-    return function() {
-        return callback(i);
-    }
-}
 
-export function iHateThis() {
-    let x = ["f you", "piece of shit"];
-    let xy = x.forEach(callbackClosure(item, function(item) {return item + "dude"})
-    );  
-}
+
